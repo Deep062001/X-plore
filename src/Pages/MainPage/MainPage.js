e85487539fd2802ab9470966dc30c403b916afc0
Deepanshi Singh <dee062001@gmail.com>
2022-02-20 03:01:06 +0530

    made side bar resizable, using npm re-resizable

--- src/Pages/MainPage/MainPage.js
+++ src/Pages/MainPage/MainPage.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react'
 import AddDropdown from '../../components/AddDropdown/AddDropdown';
 import AddFileFolderModal from '../../components/AddFileFolderModal/AddFileFolderModal';
 import EditFileModal from '../../components/EditFileModal/EditFileModal';
+import FileElement from '../../components/FileElement/FileElement';
 import Header from '../../components/Header/Header';
 import LeftExplorer from '../../components/LeftExplorer/LeftExplorer';
 import PinModal from '../../components/PinModal/PinModal';
 import SetNewPinModal from '../../components/SetNewPinModal/SetNewPinModal';
+import { Resizable } from "re-resizable";
 import './MainPage.scss';
 
 const MainPage = () => {
@@ -32,10 +34,19 @@ const MainPage = () => {
   return (
     <div className='main-page-div'>      
         <div className='left-div-main-page'>
+            <Resizable>
             <LeftExplorer showPinModalFunc={handleShowPinModal} showAddFileFolderModalFunc={handleShowAddFileFolderModal}/>
+            </Resizable>
         </div>
         <div className='right-div-main-page'>
+          <div>
             <Header showAddFileFolderModalFunc={handleShowAddFileFolderModal} showChangePinFunc={handleShowChangePinModal}/>
+          </div>
+          <div>
+            <FileElement/>
+            <FileElement/>
+            <FileElement/>
+          </div>
         </div>
         {/* <SetNewPinModal/> */}
 
---
+++ src/components/FileElement/FileElement.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import FileIcon from '../../assets/FileIcon.svg';
+const FileElement = () => {
+  return (
+    <div>
+        <img src={FileIcon}/>
+    </div>
+  )
+}
+
+export default FileElement
